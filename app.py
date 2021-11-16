a21c9061ce3b701f2d8f029ff3c0a2787c3e40f8
soumodiptab <sbose2019@gmail.com>
2021-11-17 02:22:47 +0530

    Calendar display done

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required, UserMixin
+from flask_login import UserMixin
 import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eval.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/eval'
 app.config['SECRET_KEY'] = 'secretkey'
 
 db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+
 
 class Event(db.Model):
     __tablename__ = 'event'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    date = db.Column(db.String(80), nullable=False)
-    starttime = db.Column(db.TIMESTAMP,nullable=False)
-    endtime =
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    url = db.Column(db.String(255))
+    start = db.Column(db.DateTime, nullable=False)
+    end = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.String(20), nullable=False)
 
 
 class Admin(UserMixin, db.Model):
@@ -26,6 +26,26 @@ class Admin(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
+"""
+@app.route('/observer/event/create',methods=['GET'])
+def create_event():
+"""
+
+@app.route('/observer/event/calendar', methods=['GET'])
+def view_events():
+    events = Event.query.all()
+    calendar_populate = []
+    for event in events:
+        if event.status == 'COMPLETED':
+            color = 'green'
+        else:
+            color = 'blue'
+        new_event = {
+            'title': event.name, 'start': event.start, 'end': event.end, 'backgroundColor': color
+        }
+        calendar_populate.append(new_event)
+    return render_template('calender.html', events=calendar_populate)
+
 
 if (__name__ == '__main__'):
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
